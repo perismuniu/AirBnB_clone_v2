c71eb71d7c8bd474334e5e0b14fa740160cd08ff
perismuniu <peris.muniu@gmail.com>
2023-11-20 08:03:49 -0800

    correction 3. MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
 -- Script that prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
