b572eb6fc4088d4167fa9984a588db1a2e817573
perismuniu <peris.muniu@gmail.com>
2023-12-06 11:07:03 -0800

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,30 @@
-#!/usr/bin/env python3
-"""Fabric script to distribute an archive to web servers"""
+#!/usr/bin/python3
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-from fabric.api import env, put, run, sudo
+from fabric.api import put, run, env
 from os.path import exists
-from datetime import datetime
+env.hosts = ['52.90.15.100', '54.89.59.115']
 
-env.hosts = ['<IP web-01>', 'IP web-02']
-env.user = '<your_ssh_username>'
-env.key_filename = '<path_to_your_ssh_private_key>'
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        # Upload the archive to /tmp/ directory on the web server
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-
-        # Extract archive to /data/web_static/releases/<archive_filename>
-        archive_filename = archive_path.split("/")[-1]
-        release_path = '/data/web_static/releases/{}'.format(archive_filename[:-4])
-        run('mkdir -p {}'.format(release_path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
-
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(archive_filename))
-
-        # Delete the symbolic link /data/web_static/current
-        current_link = '/data/web_static/current'
-        run('rm -f {}'.format(current_link))
-
-        # Create a new symbolic link /data/web_static/current
-        run('ln -s {} {}'.format(release_path, current_link))
-
-        print("New version deployed!")
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
-    except Exception as e:
-        print("Deployment failed: {}".format(str(e)))
+    except:
         return False
-
-if __name__ == "__main__":
-    archive_path = "path/to/your/archive.tgz"  # Replace with the actual path to your archive
-    do_deploy(archive_path)
